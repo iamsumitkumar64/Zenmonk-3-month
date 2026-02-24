e290cc48e179b366361d95d2cbbedeb2139832e8
iamsumitkumar64 <iam.sumitkumar64@gmail.com>
2026-02-24 14:44:47 +0530

    parameter-fixed

--- day 1 firebase/backend/src/infrastructure/google-strategy/google.strategy.ts
+++ day 1 firebase/backend/src/infrastructure/google-strategy/google.strategy.ts
@@ -16,13 +16,13 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
         });
     }
 
-    async validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
+    async validate(request: any, accessToken: string, refreshToken: string, profile: any, done: VerifyCallback,): Promise<any> {
         const { name, emails, photos } = profile;
         const user = {
-            email: emails[0].value,
-            firstName: name.givenName,
-            lastName: name.familyName,
-            picture: photos[0].value,
+            email: emails?.[0]?.value,
+            firstName: name?.givenName,
+            lastName: name?.familyName,
+            picture: photos?.[0]?.value,
             accessToken,
         };
         done(null, user);
