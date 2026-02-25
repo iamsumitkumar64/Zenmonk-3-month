f9c997f1b929f6b40505adf91fdbd4e5f2810edb
iamsumitkumar64 <iam.sumitkumar64@gmail.com>
2026-02-25 17:58:28 +0530

    css,utils,duration and current time shown

--- music player/frontend/src/components/Player/page.tsx
+++ music player/frontend/src/components/Player/page.tsx
@@ -2,9 +2,10 @@
 import { useEffect, useState, useRef } from 'react';
 import './player.css'
 import { useAudioPlayer } from '@/context/track';
-import { generateRandom3DigitNumber } from '@/utils/random';
+import { generateRandom3DigitNumberUtil } from '@/utils/random';
 import { useSnackbar } from 'notistack';
 import Image from 'next/image';
+import { timeConversionUtil } from '@/utils/time.conversion';
 
 export default function PlayerComp() {
     const { audioUrl, nextTrack, prevTrack, setAudioUrl } = useAudioPlayer();
@@ -12,9 +13,11 @@ export default function PlayerComp() {
     const [isPlaying, setIsPlaying] = useState<boolean>(false);
     const audioRef = useRef<HTMLAudioElement>(null);
     const { enqueueSnackbar } = useSnackbar();
+    const [currentTime, setcurrentTime] = useState<any>(0);
+    const [duration, setduration] = useState<any>(0);
 
     useEffect(() => {
-        const random = generateRandom3DigitNumber();
+        const random = generateRandom3DigitNumberUtil();
         setImg(`https://picsum.photos/${random}/${random}.jpg`);
         setIsPlaying(false);
         enqueueSnackbar("Audio Selected =>" + audioUrl, { variant: "success" });
@@ -22,7 +25,9 @@ export default function PlayerComp() {
 
     const togglePlay = () => {
         if (!audioRef.current) return;
-
+        console.log(audioRef);
+        setcurrentTime(timeConversionUtil(audioRef.current.currentTime));
+        setduration(timeConversionUtil(audioRef.current.duration));
         if (isPlaying) {
             audioRef.current.pause();
         } else {
@@ -31,7 +36,9 @@ export default function PlayerComp() {
         setIsPlaying(!isPlaying);
     };
 
-    if (!audioUrl) return <>No Files Exists ...</>
+    if (!audioUrl) {
+        enqueueSnackbar("No Audio right now =>" + audioUrl, { variant: "error" });
+    }
 
     const changeAudio = (url: string | null) => {
         setAudioUrl(url);
@@ -43,8 +50,12 @@ export default function PlayerComp() {
             <li>
                 <audio ref={audioRef} src={'/uploads/' + audioUrl} />
                 <p>{audioUrl}</p>
+                <div>
+                    <p>{currentTime}</p>
+                    <p>{duration}</p>
+                </div>
                 <div className="controls">
-                    <button onClick={() => changeAudio(prevTrack)}>
+                    <button onClick={() => changeAudio(prevTrack)} className='.specificButton'>
                         <Image
                             src="/prev_track.png"
                             width={500}
@@ -52,7 +63,7 @@ export default function PlayerComp() {
                             alt="Picture of the author"
                         />
                     </button>
-                    <button onClick={togglePlay}>
+                    <button onClick={togglePlay} className='.specificButton'>
                         {isPlaying ? <Image
                             src="/play.png"
                             width={500}
@@ -65,7 +76,7 @@ export default function PlayerComp() {
                             alt="Picture of the author"
                         />}
                     </button>
-                    <button onClick={() => changeAudio(nextTrack)}>
+                    <button onClick={() => changeAudio(nextTrack)} className='.specificButton'>
                         <Image
                             src="/next_track.png"
                             width={500}
